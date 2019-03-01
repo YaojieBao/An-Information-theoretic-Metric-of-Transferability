44510faf911f329f6c4c90f96f6bf025adf10ffa
YaojieBao <yaojiebaosincere@gmail.com>
2019-03-01 14:33:05 +0800

    Update transfer_feature_4a-4f.py
    To extract feature for all the 6 parts of Cifar-100 data in one script.

--- validate_H-score/transfer_feature_4a-4f.py
+++ validate_H-score/transfer_feature_4a-4f.py
@@ -25,29 +25,30 @@ f.close()
 '''
 from keras.datasets import cifar100
 (X_train, y_train), (X_test, y_test) =cifar100.load_data(label_mode='fine')
-# For the limited memory capacity, features are extracted part by part. 
-X_train = np.array([resize(x,(224,224)) for x in X_train[:10000]], dtype=np.float32)
+X_train = np.concatenate((X_test,X_train),axis=0)
 
 alphabet = [chr(i) for i in range(97,123)]
+# For the limited memory capacity, features are extracted part by part. 
+for j in range(6):
+    for i, k in enumerate(np.linspace(25, 40, 6)):
+        clear_session()
 
-for i, k in enumerate(np.linspace(25, 40, 6)):
-    clear_session()
+        input_tensor = Input(shape=(224, 224, 3))
 
-    input_tensor = Input(shape=(224, 224, 3))
-    
-    base_model = ResNet50(input_tensor=input_tensor, weights='imagenet', include_top=False)
-    layer_name = 'activation_'+str(int(k))
-    x = base_model.get_layer(layer_name).output
-    
-    feature = AveragePooling2D((14, 14), name='avg_pool')(x)
-    model = Model(inputs=base_model.input, outputs=feature)
-    
-    resnet50_train_output = model.predict(X_train)
-    
-    file_name = os.path.join(pic_dir_out,'resnet50_train_4'+alphabet[i]+'_output_p1'+'.h5')
-    
-    f = h5py.File(file_name,'w')          
-    f.create_dataset('resnet50_train_4'+alphabet[i]+'_output_p1', data = resnet50_train_output)
-    f.close()
-    
-    print('resnet50_train_4'+alphabet[i]+'_output is done!')
+        base_model = ResNet50(input_tensor=input_tensor, weights='imagenet', include_top=False)
+        layer_name = 'activation_'+str(int(k))
+        x = base_model.get_layer(layer_name).output
+
+        feature = AveragePooling2D((14, 14), name='avg_pool')(x)
+        model = Model(inputs=base_model.input, outputs=feature)
+       
+        X_train_p = np.array([resize(x,(224,224)) for x in X_train[j*10000:(j+1)*10000]], dtype=np.float32)
+        resnet50_train_output = model.predict(X_train_p)
+
+        file_name = os.path.join(pic_dir_out,'resnet50_train_4'+alphabet[i]+'_output_p'+str(j)+'.h5')
+
+        f = h5py.File(file_name,'w')          
+        f.create_dataset('resnet50_train_4'+alphabet[i]+'_output_p'+str(j), data = resnet50_train_output)
+        f.close()
+
+        print('resnet50_train_4'+alphabet[i]+'_output_p'+str(j)+' is done!')
